53e78b58067b2721594b961609995351bad14153
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-28 20:36:40 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_05_28_202914) do
+
+  create_table "dogs", force: :cascade do |t|
+    t.string "name"
+    t.string "breed"
+  end
 
 end
